37d2b7ee17170153df69e05cd4d5bb98fd7ed71e
Michael Hoffer <info@michaelhoffer.de>
2015-06-26 17:41:02 +0200

    dependencies updated (groovy, richtextfx)

--- pow-lab/src/main/java/eu/mihosoft/pow/lab/Main.java
+++ pow-lab/src/main/java/eu/mihosoft/pow/lab/Main.java
@@ -525,7 +525,7 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
 
         FXMLLoader loader = new FXMLLoader(
                 getClass().getResource("LabScreen.fxml"));
